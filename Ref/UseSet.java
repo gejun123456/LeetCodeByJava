112b2e389e85e6b1a9ff16c3a2f15ffcd65dc386
gejun123456 <gejunxxx@163.com>
2014-11-03 00:41:18 +0800

    add map

--- Ref/UseSet.java
+++ Ref/UseSet.java
@@ -8,22 +8,21 @@ import java.util.*;
 public class UseSet {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        Set set=new HashSet();
+        Set set = new HashSet();
         set.add("abc");
         set.add("cde");
         set.add("efg");
         set.add("fgh");
         set.add("abc"); //重复的abc,set会自动将其去掉
-        System.out.println("size="+ set.size() );
+        System.out.println("size=" + set.size());
         List list = new ArrayList();
         list.add("abc");
         list.add("aaa");
         list.add("fff");
         set.addAll(list); //将list中的值加入set,并去掉重复的abc
-        System.out.println("size="+ set.size() );
-        for( Iterator it = set.iterator();  it.hasNext(); )
-        {
-            System.out.println("value="+it.next().toString());
+        System.out.println("size=" + set.size());
+        for (Iterator it = set.iterator(); it.hasNext(); ) {
+            System.out.println("value=" + it.next().toString());
         }
     }
 }
