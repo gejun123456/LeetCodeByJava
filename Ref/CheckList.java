00e45c7c45857df58148d0d31932b33b8a6b59c4
bruce.ge <gejunxxx@163.com>
2014-11-02 23:05:41 +0800

    add sub dir

--- Ref/CheckList.java
+++ Ref/CheckList.java
@@ -1,3 +1,5 @@
+package Ref;
+
 import java.util.ArrayList;
 import java.util.List;
 
--- Ref/RefactorWordLadder2.java
+++ Ref/RefactorWordLadder2.java
@@ -1,4 +1,4 @@
-import com.sun.org.apache.xpath.internal.operations.Bool;
+package Ref;
 
 import java.util.*;
 
