c7ffdfae881d1b4c07f13e52c3fdd112d520e2fa
gejun123456 <gejunxxx@163.com>
2014-11-10 21:35:06 +0800

    add 3
    1 need think again

---
+++ MedianofTwoSortedArrays.java
@@ -0,0 +1,17 @@
+/**
+ * Created by bruce on 14-11-10.
+ */
+public class MedianofTwoSortedArrays {
+    public double findMedianSortedArrays(int A[], int B[]) {
+        return 0;
+
+    }
+
+    public static void main(String[] args) {
+        int A[] = {1,2,3,4,5,6,7,9};
+        int B[] = {6,7,8,9,10,12,45,57,3423};
+        MedianofTwoSortedArrays me = new MedianofTwoSortedArrays();
+        double result = me.findMedianSortedArrays(A,B);
+        System.out.println(result);
+    }
+}
