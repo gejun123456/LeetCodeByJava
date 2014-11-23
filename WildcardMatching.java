1b4bf7d746f3af9ee24dc5d2decd66c73c06ff1c
gejun123456 <gejunxxx@163.com>
2014-11-23 20:49:01 +0800

    add WildcardMatching

--- WildcardMatching.java
+++ WildcardMatching.java
@@ -1,21 +1,229 @@
-/**
- * Created by bruce on 14-11-22.
- */
+///**
+//* Created by bruce on 14-11-23.
+//*/
 public class WildcardMatching {
     public boolean isMatch(String s, String p) {
+        int len1 = s.length();
+        int count = countNum(p);
+        if(len1<count) {
+            return false;
+        }
         p = removeDuplicate(p);
-        int noDuplicat = cal(p);
-        return matchReg(s, p, noDuplicat);
+        int len2 = p.length();
+        int firstM = -1;
+        int lastM = len2;
+        int judge = 0;
+        for (int i = 0; i < len2; i++) {
+            if (p.charAt(i) == '*'&&judge==0) {
+                judge = 1;
+                firstM = i;
+                lastM = i;
+            } else if(p.charAt(i)=='*'&&judge==1) {
+                lastM = i;
+            }
+        }
+        if (firstM == -1) {
+            if (same(s, p)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        if (firstM == lastM) {
+            if (len2 == 1) {
+                return true;
+            } else {
+                if (firstM > 0 && firstM < len2-1) {
+                    if (len1 >= firstM && len1 >= len2 - 1 && same(s.substring(0, firstM),p.substring(0, firstM)) && same(s.substring(len1 - len2 + firstM + 1, len1),p.substring(firstM + 1, len2))) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                } else if (firstM == 0) {
+                    if (len1 >= len2 - firstM - 1 && same(s.substring(len1 - len2 + firstM + 1, len1),p.substring(firstM + 1, len2))) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                } else {
+                    if (len1 >= firstM && same(s.substring(0,firstM), p.substring(0, firstM))) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        } else {
+            int start = firstM;
+            int end = lastM;
+            if (firstM != 0) {
+                if (len1 >= firstM && same(s.substring(0, firstM),p.substring(0, firstM))) {
+                    start = firstM;
+                } else {
+                    return false;
+                }
+            }
+            if (lastM != len2 - 1) {
+                if (len1 >= len2 - lastM - 1 && same(s.substring(len1 - len2 + lastM + 1, len1),p.substring(lastM + 1, len2))) {
+                    end = lastM;
+                } else {
+                    return false;
+                }
+            }
+            p = p.substring(start, end+1);
+            s = s.substring(start, len1 - len2 + end+1);
+            return matchReg(p, s);
+        }
     }
 
-    private int cal(String p) {
+    private int countNum(String p) {
         int result = 0;
         for (int i = 0; i < p.length(); i++) {
-            if (p.charAt(i) != '*') {
+            if(p.charAt(i)!='*') {
                 result++;
             }
         }
         return result;
+    }
+
+    private boolean matchReg(String p, String s) {
+        String[] res = new String[p.length()];
+        int k = 0;
+        String cur = "";
+        for (int i = 1; i < p.length(); i++) {
+            if (p.charAt(i) == '*') {
+                res[k++] = cur;
+                cur = "";
+            } else {
+                cur += p.charAt(i);
+            }
+        }
+        return matchByArr(0, k - 1, res, s);
+    }
+
+    private int findMax(String[] res, int k) {
+        int max = 0;
+        int position = 0;
+        for (int i = 0; i < k; i++) {
+            if (res[i].length() > max) {
+                max = res[i].length();
+                position = i;
+            }
+        }
+        return position;
+    }
+
+//    private void reverseArr(String[] res, int k) {
+//        String[] u = new String[k];
+//        int m = 0;
+//        for (int i = k - 1; i >= 0; i--) {
+//            u[m++] = res[i];
+//        }
+//        for (int i = 0; i < k; i++) {
+//            res[i] = u[i];
+//        }
+//    }
+//
+//    private String reverse(String s) {
+//        String p = "";
+//        for (int i = s.length() - 1; i >= 0; i--) {
+//            p += s.charAt(i);
+//        }
+//        return p;
+//    }
+
+
+    private boolean matchByArr(int g, int k, String[] res, String s) {
+        int len1 = s.length();
+        String cur = res[g];
+        int len2 = cur.length();
+        int min = 0;
+        for (int j = 0; j <= k; j++) {
+            min += res[j].length();
+        }
+        if (len1 < min) {
+            return false;
+        }
+        if (g == k) {
+            for (int j = 0; j <= len1 - len2; j++) {
+                if (same(s.substring(j, j + len2), cur)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        int max = findMax(res, k+1);
+        cur = res[max];
+        len2 = cur.length();
+        if (max == 0) {
+            String[] res1 = new String[k];
+            for (int i = 0; i < k; i++) {
+                res1[i] = res[i+1];
+            }
+            for (int j = 0; j <= len1 - len2; j++) {
+                if (same(s.substring(j, j + len2), cur)) {
+                    boolean c = matchByArr(g,k-1,res1,s.substring(j+len2,len1));
+                    if(c==true){
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        if (max == k) {
+            String[] res2 = new String[k];
+            for (int i = 0; i < k; i++) {
+                res2[i] = res[i];
+            }
+            for (int i = 0; i <= len1 - len2; i++) {
+                if(same(s.substring(i,i+len2),cur)) {
+                    boolean c = matchByArr(g,k-1,res2,s.substring(0,i));
+                    if(c==true){
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        String maxString = res[max];
+        String[] res1 = new String[max];
+        for (int i = 0; i < max; i++) {
+            res1[i] = res[i];
+        }
+        String[] res2 = new String[k - max];
+        for (int i = max + 1; i <= k; i++) {
+            res2[i - max - 1] = res[i];
+        }
+        len2 = maxString.length();
+        if (len1 < len2) {
+            return false;
+        }
+        for (int j = 0; j <= len1 - len2; j++) {
+            if (same(s.substring(j, j + len2), maxString)) {
+                boolean c = matchByArr(0, max - 1, res1, s.substring(0, j));
+                boolean d = matchByArr(0, k - max - 1, res2, s.substring(j + len2, len1));
+                if (c == true && d == true) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    private boolean same(String s, String p) {
+        int len1 = s.length();
+        int len2 = p.length();
+        if (len1 != len2) {
+            return false;
+        }
+        for (int i = 0; i < len1; i++) {
+            if (p.charAt(i) != '?' && p.charAt(i) != s.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private String removeDuplicate(String p) {
@@ -33,93 +241,13 @@ public class WildcardMatching {
         return s;
     }
 
-    private boolean matchReg(String s, String p, int noDuplicat) {
-        int len2 = p.length();
-        int len1 = s.length();
-        if (len1 < noDuplicat) {
-            return false;
-        }
-        if (len2 == 0) {
-            if (len1 == 0) {
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            char x = p.charAt(0);
-            if (x == '?') {
-                if (len1 == 0) {
-                    return false;
-                } else {
-                    return matchReg(subTime(s, 1, len1), subTime(p, 1, len2), noDuplicat - 1);
-                }
-            } else if (x == '*') {
-                if (noDuplicat == 0) {
-                    return true;
-                }
-                for (int i = 0; i <= len1 - noDuplicat; i++) {
-                    if (len2 > 1) {
-                        if (p.charAt(1) == '?') {
-                            boolean c = matchReg(s.substring(i, len1), subTime(p,1,len2), noDuplicat - 1);
-                            if (c == true) {
-                                return true;
-                            }
-                        } else {
-                            int g = 1;
-                            int k = -1;
-                            int m = i;
-                            while (g < p.length() && m < s.length() && p.charAt(g) != '*' && p.charAt(g) != '?') {
-                                if (s.charAt(m++) != p.charAt(g++)) {
-                                    k = 0;
-                                    break;
-                                }
-                            }
-                            if (k == 0) {
-                                continue;
-                            } else {
-                                boolean c = matchReg(subTime(s, m, len1), subTime(p, g, len2), noDuplicat - m - i);
-                                if (c == true) {
-                                    return true;
-                                }
-                            }
-                        }
-                    } else {
-                        return true;
-                    }
-
-                }
-                return false;
-            } else {
-                if (len1 == 0) {
-                    return false;
-                } else {
-                    int g = 0;
-                    int m = 0;
-                    while (g < p.length() && p.charAt(g) != '*' && p.charAt(g) != '?') {
-                        if (s.charAt(m++) != p.charAt(g++)) {
-                            return false;
-                        }
-                    }
-                    return matchReg(subTime(s, m, len1), subTime(p, g, len2), noDuplicat - m - 0);
-                }
-
-            }
-        }
-    }
-
-
-    private String subTime(String p, int g, int len2) {
-        if (g < len2) {
-            return p.substring(g, len2);
-        } else {
-            return "";
-        }
-    }
-
     public static void main(String[] args) {
-        String a = "bbaaaabaaaaabbabbabbabbababaabababaabbabaaabbaababababbabaabbabbbbbbaaaaaabaabbbbbabbbbabbabababaaaaa";
-        String b = "******aa*bbb*aa*a*bb*ab***bbba*a*babaab*b*aa*a****";
+        long start = System.currentTimeMillis();
+        String a = "aaabbbabbababaababbbbbabbaaaaabbbbaaaabaaaabbabbbaaababaaaabababbaababbaabbbbaabaabaababbbabaabbabbaaaaaabbbabaabaaabbbaabbabaabbbbabbabaababbbbbbbaaaaababaabbaaaabbabbaaaabbbbaabbaabbaababaabaabbbaab";
+        String b = "******a****aa**a**aa**aa**ba**aba*aa*b*a*bb**a*****b*******bb******a*aa*bbba*a*ba***baa*a*abb***ab*ab";
         boolean result = new WildcardMatching().isMatch(a, b);
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
         System.out.println(result);
     }
 }
