196bc86667b007bc6aca31651cecd4ef1c7af26a
gejun123456 <gejunxxx@163.com>
2014-12-08 20:51:49 +0800

    fix this problem tonight

---
+++ PalindromePartitioningII.java
@@ -0,0 +1,15 @@
+/**
+ * Created by bruce on 14-12-8.
+ */
+public class PalindromePartitioningII {
+    public int minCut(String s) {
+        int result = 0;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String s = "abacaba";
+        int result = new PalindromePartitioningII().minCut(s);
+        System.out.println(result);
+    }
+}
